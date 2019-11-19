048b6f7e06a237c6532bae4b347317ab610995ea
Andreww8xx8 <kupertfail1498@gmail.com>
2019-11-19 19:05:27 +0300

    array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,42 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        i = 0
+        new_array = []
+        max_in_the_array = array.max
+        while i < array.size
+          if array[i] > 0
+            new_array[i] = max_in_the_array
+          else
+            new_array[i] = array[i]
+          end
+            i += 1
+        end
+        new_array
       end
+    
 
-      def search(_array, _query)
-        0
+       def search(_array, _query)
+        low = 0
+        high = _array.length - 1
+        if low > high
+          -1 
+        else
+        while low <= high
+          mid = low + (high - low) / 2
+          guess = _array[mid]
+           if (_query < guess)
+             high = mid - 1
+           elsif (_query > guess)
+             low = mid + 1
+           else
+             break mid  
+           end        
+           if low > high
+            break -1    
+           end 
+         end
+        end
       end
     end
   end
