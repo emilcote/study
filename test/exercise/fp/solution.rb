0b45a29e9bfe36e0075ddb7b969bdc34ee123f73
Andreww8xx8 <kupertfail1498@gmail.com>
2019-11-21 19:03:14 +0300

    first

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,7 +5,11 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        b = _array.select {|film| "#{film["country"]}".include? "," && !"#{film["rating_kinopoisk"]}".include? "0" && !"#{film["rating_kinopoisk"]}".include? "nil" }
+        
+          
+      
+        puts b
       end
 
       def chars_count(_films, _threshold)
